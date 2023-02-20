73ed092f26f5b8417249ed2334bf7d07c9f1f5ee
ld-npjx <1195688157@qq.com>
2023-02-20 11:21:22 +0800

    https://github.com/ld-npjx/Flink.git

---
+++ src/main/java/com/atguigu/wc/StreamWordCount.java
@@ -0,0 +1,19 @@
+package com.atguigu.wc;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+
+public class StreamWordCount {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment ev = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        String inputPath="C:\\JavaCode\\Flink\\scr\\main\\resources\\test.txt";
+        DataStream<String> dataSource = ev.readTextFile(inputPath);
+
+
+    }
+
+}
